49db1d517a3dc099a57c14a5ce7f26597e13bf1a
tomasz <rogala.tomasz1@gmail.com>
2024-10-16 14:26:03 +0200

    Multiplatform fix: define export macros for components on Unix-based systems

--- include/ExportDefinitions.hpp
+++ include/ExportDefinitions.hpp
@@ -1,5 +1,6 @@
 ﻿#pragma once
 
+#ifdef _WIN32
 #ifdef BUILD_CONTENT_METADATA_API
 #define CONTENT_METADATA_API_EXPORT __declspec(dllexport)
 #else
@@ -16,4 +17,23 @@
 #define CONTENT_METADATA_REPOSITORY_EXPORT __declspec(dllexport)
 #else
 #define CONTENT_METADATA_REPOSITORY_EXPORT __declspec(dllimport)
+#endif
+#else
+#ifdef BUILD_CONTENT_METADATA_API
+#define CONTENT_METADATA_API_EXPORT __attribute__ ((visibility ("default")))
+#else
+#define CONTENT_METADATA_API_EXPORT
+#endif
+
+#ifdef BUILD_CONTENT_METADATA_CORE
+#define CONTENT_METADATA_CORE_EXPORT __attribute__ ((visibility ("default")))
+#else
+#define CONTENT_METADATA_CORE_EXPORT
+#endif
+
+#ifdef BUILD_CONTENT_METADATA_REPOSITORY
+#define CONTENT_METADATA_REPOSITORY_EXPORT __attribute__ ((visibility ("default")))
+#else
+#define CONTENT_METADATA_REPOSITORY_EXPORT
+#endif
 #endif
